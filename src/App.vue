1a0baa70762ef3d8daca98782bfc557c51cfb06a
Theo Manton <theo333@gmail.com>
2020-08-11 19:01:34 -0400

    removed old API key

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" v-bind:class="getBackgroundImgClass(this.backgroundImgClass)">
     <main>
       <div class="search-box">
         <input type="text" class="search-input" placeholder="Enter city name..." v-model="query" />
@@ -18,8 +18,10 @@
             {{ Math.round(weatherResults.main.temp) }}&deg;
             {{ this.units === "imperial" ? " F" : " C" }}
           </div>
-          <div class="weather">{{ weatherResults.weather[0].main }}</div>
-          <img v-bind:src="createImgUrl(weatherResults.weather[0].icon)" />
+          <div class="conditions">
+            <div class="weather-description">{{ weatherResults.weather[0].description }}</div>
+            <img v-bind:src="createImgUrl(weatherResults.weather[0].icon)" />
+          </div>
         </div>
       </div>
     </main>
@@ -28,34 +30,54 @@
 
 <script>
 import axios from "axios";
-// http://openweathermap.org/img/wn/10d@2x.png
 
 export default {
   name: "App",
   data() {
     return {
-      api_Key: "6fe9efe3e60884da2f8b6c17c0be24f8",
       base_url: "https://api.openweathermap.org/data/2.5/",
       img_base_url: "http://openweathermap.org/img/wn/",
       img_base_url_suffix: "@2x.png",
       units: "imperial",
       query: "",
       weatherResults: {},
+      backgroundImgClass: "",
     };
   },
   methods: {
     getWeather() {
       axios
         .get(
-          `${this.base_url}weather?q=${this.query}&APPID=${this.api_Key}&units=${this.units}`
+          `${this.base_url}weather?q=${this.query}&APPID=${process.env.VUE_APP_API_KEY}&units=${this.units}`
         )
         .then((resp) => {
           this.weatherResults = resp.data;
+          this.backgroundImgClass = this.weatherResults.weather[0].main;
           console.log(this.weatherResults);
         });
     },
     createImgUrl(icon) {
       return this.img_base_url + icon + this.img_base_url_suffix;
+    },
+    getBackgroundImgClass(conditions) {
+      let className = "";
+      switch (conditions) {
+        case "Clear":
+          className = "sunny";
+          break;
+        case "Clouds":
+          className = "cloudy";
+          break;
+        case "Rain":
+          className = "rainy";
+          break;
+        case "Mist":
+          className = "mist";
+          break;
+        default:
+          return "";
+      }
+      return className;
     },
   },
 };
@@ -79,6 +101,22 @@ export default {
   background-position: top;
 }
 
+#app.sunny {
+  background-image: url("./assets/images/sunny/kilarov-zaneit-OzRhGl0BsWU-unsplash.jpg");
+}
+
+#app.cloudy {
+  background-image: url("./assets/images/cloudy/fabrizio-conti-AtHvihIObKo-unsplash.jpg");
+}
+
+#app.rainy {
+  background-image: url("./assets/images/rainy/neonbrand-TtlSPDneJgM-unsplash.jpg");
+}
+
+#app.mist {
+  background-image: url("./assets/images/mist/panjinda-iIqiI5fyZKs-unsplash.jpg");
+}
+
 main {
   display: flex;
   flex-direction: column;
@@ -87,8 +125,8 @@ main {
   transition: 0.5s;
   background-image: linear-gradient(
     to bottom,
-    rgba(0, 0, 0, 0.05),
-    rgba(0, 0, 0, 0.15)
+    rgba(0, 0, 0, 0.15),
+    rgba(0, 0, 0, 0.35)
   );
   color: yellow;
 }
@@ -112,8 +150,13 @@ main {
   margin-left: 10px;
 }
 
+.location-box {
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
 .location-box .location {
   font-size: 2rem;
+  font-weight: 800;
 }
 
 .location-box .date {
@@ -122,14 +165,29 @@ main {
 
 .weather-box {
   display: inline-block;
+  /* background-color: rgba(0, 0, 0, 0.25); */
+}
+
+.weather-box .temp,
+.weather-box .conditions {
+  margin: 15px;
+  padding: 10px 15px;
+  background-color: rgba(255, 255, 255, 0.5);
 }
 
 .weather-box .temp {
+  border-radius: 15px;
   color: yellow;
-  background-color: rgba(0, 0, 0, 0.25);
-  font-size: 3rem;
-  border-radius: 10px;
-  margin: 15px;
-  padding: 5px 15px;
+  font-weight: 800;
+  font-size: 3.5rem;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .conditions {
+  /* background-color: rgba(255, 255, 255, 0.35); */
+  border-radius: 15px;
+  font-size: 1.5rem;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  margin: ;
 }
 </style>
